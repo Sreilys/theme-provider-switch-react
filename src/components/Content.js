7a9e892f256e365f14ca2199b26ca7f7af0b59f3
Sabrina Bosco <sabrina.bosco@springernature.com>
2020-12-05 16:25:42 +0100

    link to the code

--- src/components/Content.js
+++ src/components/Content.js
@@ -25,7 +25,7 @@ const Content = () => {
           <p>Hi there! I love to learn by getting hands dirty with code as soon as I read a new concept, that's why I like to build useless things, just to have fun!</p>
           <p>Is with those "useless things", which I call them experiments, that I learn the most. I sit with a tea and music, I have no pressure and let my creativity go...</p>
           <p>In this project I am playing around with React, Hooks, Styled Components, CSS Grid and Responsive mobile-first in order to fetch some very colorful placeholder thumbnails.</p>
-          <p>The UI can switch between Dark and Light Theme thanks to styled components theme provider.</p>
+          <p>The UI can switch between Dark and Light Theme thanks to styled components theme provider. Take a look at the code <a href="https://github.com/Sreilys/theme-provider-switch-react">here</a>.</p>
           {data.slice(0, 10).map(({id, title, thumbnailUrl}) => (
             <article className="photo-article" key={`photo-${id}`}>
               <Image alt={title} src={thumbnailUrl} />
